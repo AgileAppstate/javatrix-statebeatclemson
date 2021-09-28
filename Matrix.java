80518f6567f70b9be59c88b1fe836d0a72a1496c
Andrew Reeves <reevesat@appstate.edu>
2021-09-28 10:48:18 -0400

    added constructor for constant matrix

--- Matrix.java
+++ Matrix.java
@@ -1,8 +1,19 @@
 public class Matrix{
 	//Constructors
 	Matrix(int m, int n) {
-		this.m = m;
-		this.n = n;
-		mat = new double[m][n];	
+	    this.m = m;
+	    this.n = n;
+	    mat = new double[m][n];	
 	}
+	Matrix(int m, int n, double s) {
+	    this.m = m;
+	    this.n = n;
+	    mat = new double[m][n];
+	    for (int i = 0; i < m; i++) {
+		for (int j = 0 ; j < n; j++) {
+		    mat[i][j] = s;
+	   	}
+	    }
+	}
+
 }
