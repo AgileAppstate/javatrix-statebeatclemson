614730c542f345587c0a43a2bdec7390b91f5fb8
Andrew Reeves <reevesat@appstate.edu>
2021-09-28 11:10:20 -0400

    added constructor for matrix from 2D array

--- Matrix.java
+++ Matrix.java
@@ -1,11 +1,11 @@
 public class Matrix{
 	//Constructors
-	Matrix(int m, int n) {
+	public Matrix(int m, int n) {
 	    this.m = m;
 	    this.n = n;
 	    A = new double[m][n];	
 	}
-	Matrix(int m, int n, double s) {
+	public Matrix(int m, int n, double s) {
 	    this.m = m;
 	    this.n = n;
 	    A = new double[m][n];
@@ -15,5 +15,16 @@ public class Matrix{
 	   	}
 	    }
 	}
+	public Matrix(double[][] A) {
+	    m = A.length;
+	    n = A[0].length;
+	    for (int i = 0; i < m; i++) {
+		if (A[i].length != n) {
+		    throw new IllegalArgumentException("All rows must have the same length.");
+		}
+	    }
+	    this.A=A;
+	}
+	
 
 }
