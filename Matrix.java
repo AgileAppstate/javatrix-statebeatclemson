489307bea1742ca7dfb6d72e9505501dadbc1b56
Andrew Reeves <reevesat@appstate.edu>
2021-09-28 11:05:38 -0400

    adjusted matrix naming

--- Matrix.java
+++ Matrix.java
@@ -3,15 +3,15 @@ public class Matrix{
 	Matrix(int m, int n) {
 	    this.m = m;
 	    this.n = n;
-	    mat = new double[m][n];	
+	    A = new double[m][n];	
 	}
 	Matrix(int m, int n, double s) {
 	    this.m = m;
 	    this.n = n;
-	    mat = new double[m][n];
+	    A = new double[m][n];
 	    for (int i = 0; i < m; i++) {
 		for (int j = 0 ; j < n; j++) {
-		    mat[i][j] = s;
+		    A[i][j] = s;
 	   	}
 	    }
 	}
