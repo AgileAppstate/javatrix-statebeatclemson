baa2d67fa50ab13597dc64b096186ababe6178fd
Andrew Reeves <reevesat@appstate.edu>
2021-09-30 14:25:31 -0400

    added first two get submatrix methods

--- javatrix/Matrix.java
+++ javatrix/Matrix.java
@@ -176,5 +176,37 @@ public class Matrix{
 	    return A[i][j];
 	}
 	
+	public Matrix getMatrix(int i0, int i1, int j0, int j1) { 
+	    Matrix Z = new Matrix(i1-i0+1. j1-j0+1);
+	    double[][] B =Z.getArray();
+	    try {
+		for (int i = i0; i <= i1; i++) {
+		    for (int j = j0; j <= j1; j++) {
+			B[i-i0][j-j0] = A[i][j];
+		    }
+		}
+	    }
+	    catch (ArrayIndexOutOfBoundsException e) {
+		throw new ArrayIndexOutOfBoundsException("Submatrix indices");
+	    }
+	    return Z;
+	}
+	
+	public Matrix getMatrix(int[] r, int[] c) {
+	    Matrix Z = new Matrix(r.length, c.length);
+	    double[][] B = Z.getArray();
+	    try {
+		for (int i = 0; i < r.length; i++) {
+		    for (int j = 0; j < c.length; j++) {
+			B[i][j] = A[r[i]][c[j]];
+		    }
+		}
+	    }
+	    catch (ArrayIndexOutOfBoundsException e) {
+		throw new ArrayIndexOutOfBoundsException("Submatrix indices");
+	    }
+	    return Z;
+	} 
 
+	public Matrix getMatrix(
 }
