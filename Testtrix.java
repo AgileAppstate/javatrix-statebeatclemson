68b815f645a9c36d35d1911bda3ab42c26873676
Andrew Reeves <reevesat@appstate.edu>
2021-10-05 11:08:46 -0400

    started menu functionality

--- Testtrix.java
+++ Testtrix.java
@@ -1,20 +1,47 @@
 import javatrix.*;
+import java.util.Scanner;
 
 public class Testtrix {
     public static void main(String[] args) {
-        double[][] vals = {{1.,2.,3},{4.,5.,6.},{7.,8.,9.}};
 
-        Matrix A = new Matrix(vals);
-        Matrix x = new Matrix(3, 1, 1.);
-        Matrix b = A.times(x);
+	
+        //menu to choose functionality
+	Scanner sc = new Scanner(System.in);
+	int choice = 0;
+	
+	System.out.println("---choose option---");
+	System.out.println("1. create matrix");
+	
+	choice = sc.nextInt();
+	sc.nextLine();
+	switch (choice) {
+	
+	//create matrix
+	case 1:
+		int r = 0;
+		int c = 0;
+		System.out.print("Enter number of rows: ");
+		r = sc.nextInt();
+		sc.nextLine();
 
-        A.print(9,4);
-        System.out.println("x");
-        x.print(9,4);
-        System.out.println("=");
-        b.print(9,4);
+		System.out.print("Enter number of columns: ");
+		r = sc.nextInt();
+		sc.nextLine();
 
-	/* b should be {6, 15, 24} */
-   }
+		Matrix A = new Matrix(r, c, 1.);
+		
+		System.out.println("Your matrix: ");
+		A.print(9,4);
+		break;
+
+	default:
+     	    System.out.println("Invalid selection");
+      	    break; // This break is not really necessary
+    	}
+
+	sc.close();
+    }
+
+    	
 }
 
