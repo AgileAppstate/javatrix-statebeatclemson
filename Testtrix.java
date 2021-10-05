a12b0854541214fe0997e2b512305d4341bbfdd4
Andrew Reeves <reevesat@appstate.edu>
2021-10-05 11:38:25 -0400

    fixed issue, added continue loop

--- Testtrix.java
+++ Testtrix.java
@@ -3,46 +3,51 @@ import java.util.Scanner;
 
 public class Testtrix {
     public static void main(String[] args) {
-
-	
+		
         //menu to choose functionality
 	Scanner sc = new Scanner(System.in);
 	int choice = 0;
+	boolean runAgain = true;
+
+	while(runAgain) {
+	    System.out.println("---choose option---");
+     	    System.out.println("1. create matrix");
 	
-	System.out.println("---choose option---");
-	System.out.println("1. create matrix");
-	
-	choice = sc.nextInt();
-	sc.nextLine();
-	switch (choice) {
-	
-	//create matrix
-	case 1:
-		int r = 0;
-		int c = 0;
-		System.out.print("Enter number of rows: ");
-		r = sc.nextInt();
-		sc.nextLine();
+	    choice = sc.nextInt();
+	    sc.nextLine();
+	    switch (choice) {
+	    	//create matrix
+		case 1:
+			int r = 0;
+			int c = 0;
+			System.out.print("Enter number of rows: ");
+			r = sc.nextInt();
+			sc.nextLine();
 			
-		System.out.print("Enter number of columns: ");
-		r = sc.nextInt();
-		sc.nextLine();
+			System.out.print("Enter number of columns: ");
+			c = sc.nextInt();
+			sc.nextLine();
 
-		Matrix A = new Matrix(r, c, 1.);
+			Matrix A = new Matrix(r, c);
 		
-		System.out.println(A.get(0,0));
-		System.out.println("Your matrix: ");
-		A.print(3,2);
-		break;
+			System.out.println(A.get(0,0));
+			System.out.println("Your matrix: ");
+			A.print(3,2);
+			break;
 
-	default:
-     	    System.out.println("Invalid selection");
-      	    break; // This break is not really necessary
-    	}
+		default:
+     	    		System.out.println("Invalid selection");
+      	    		break; // This break is not really necessary
+    	    }
+	    System.out.println("Continue? y or n:");
+	    String letter = sc.nextLine();
+	    if (letter.equalsIgnoreCase("y")) {
+		runAgain = true;
+	    }
+	    else runAgain = false;	
+	}
 
 	sc.close();
-    }
-
-    	
+    }    	
 }
 
